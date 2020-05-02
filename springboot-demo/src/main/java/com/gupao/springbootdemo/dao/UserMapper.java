3da9e69fb1054a2a9ac6aff393c06298279e21e0
yehanshangJLU <62952599+yehanshangJLU@users.noreply.github.com>
2020-05-02 23:12:35 +0800

    add spring boot to local

---
+++ springboot-demo/src/main/java/com/gupao/springbootdemo/dao/UserMapper.java
@@ -0,0 +1,18 @@
+package com.gupao.springbootdemo.dao;
+
+
+import com.gupao.springbootdemo.domain.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@Mapper
+public interface UserMapper {
+    User find(String username);
+    List<User> list();
+    int insert(User user);
+    int delete(int id);
+    int update(User user);
+}
---
+++ springboot-demo/src/main/java/com/gupao/springbootdemo/service/UserService.java
@@ -0,0 +1,46 @@
+package com.gupao.springbootdemo.service;
+
+import com.gupao.springbootdemo.dao.UserMapper;
+import com.gupao.springbootdemo.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserMapper userMapper;
+
+    public User findByUsername(String username){
+        return userMapper.find(username);
+    }
+
+    public List<User> listUser(){
+        return userMapper.list();
+    }
+
+    public int insertUser(User user){
+        return userMapper.insert(user);
+    }
+
+    public int updateUser(User user){
+        return userMapper.update(user);
+    }
+
+    public int delete(int id){
+        return userMapper.delete(id);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
