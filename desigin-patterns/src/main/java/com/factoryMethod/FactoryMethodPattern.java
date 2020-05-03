d9ded952d6f19ac59e9f2455dea8f9b50429bbec
yehanshangJLU <62952599+yehanshangJLU@users.noreply.github.com>
2020-05-03 19:23:24 +0800

    工厂模式

---
+++ desigin-patterns/src/main/java/com/factoryMethod/FactoryMethodPattern.java
@@ -0,0 +1,13 @@
+package com.factoryMethod;
+
+/**
+ * 工厂方法模式 （Factory Method Pattern）
+ * 定义 一个 创建对象的接口，但让 实现这个接口的类来决定 实例化哪个类
+ * 工厂方法模式  让类的实现 推迟到子类中进行
+ * 属于 创建型 设计模式
+ */
+public class FactoryMethodPattern {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ desigin-patterns/src/main/java/com/simpleFactoryPattern/SimpleFactoryPattern.java
@@ -0,0 +1,13 @@
+package com.simpleFactoryPattern;
+
+/**
+ * 简单工厂模式  Simple Factory Pattern
+ * 由一个 工厂对象决定 创建出哪一种 产品类的实例
+ * 属于 创建型 模式，不属实23种设计模式
+ */
+public class SimpleFactoryPattern {
+    public static void main(String[] args) {
+
+    }
+
+}
